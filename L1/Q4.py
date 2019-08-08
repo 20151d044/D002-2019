d95788bb097b98360854a0aaa35cb1c9f74306a7
20151d044 <53845659+20151d044@users.noreply.github.com>
2019-08-08 23:24:14 +0800

    Update Q4.py

--- L1/Q4.py
+++ L1/Q4.py
@@ -1,6 +1,6 @@
 y = input("please enter the year\n")
 if float(y)%1 != 0:
-    print ("please enter a valid year")
+    print ("invalid year")
 elif int(y)%4 == 0 :
     print ("Yes, it is a leap year")
 else:
