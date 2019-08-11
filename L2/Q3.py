f3d7000ca4dbe619df1031f9ab2cd6326600a7e1
20151d044 <53845659+20151d044@users.noreply.github.com>
2019-08-11 23:54:05 +0800

    Update Q3.py

--- L2/Q3.py
+++ L2/Q3.py
@@ -2,14 +2,9 @@ x = 0
 for n in [-120,14,93,320,1,-999]:
     if x==0:
         x = n
-    print ("%d>%d"%((max(n,x),min(n,x))
-    x = max (n,x)
 
+    if n>x :
+        x = n
+        
 
-
-
-
-
-   # if n>x:
-      #  print ("%d>%d"%(n,x))
-
+print (x)
