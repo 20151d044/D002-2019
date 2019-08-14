3dc456530e73a3b145428c67b73ef6255794183f
20151d044 <53845659+20151d044@users.noreply.github.com>
2019-08-15 01:38:28 +0800

    Update bonus.py

--- L4/bonus.py
+++ L4/bonus.py
@@ -1,16 +1,50 @@
-a1 = [5,8]
-c1 = [10,1]
-a2 = [-1,11]
-c2 = [5,8]
+def compare_x(a1,a2,c2):
+    if a2[0]<c2[0]:
+        if (a1[0] in range(a2[0],c2[0]+1)):
+            x = True
+        else:
+            x = False
+    if a2[0]>c2[0]:
+        if (a1[0] in range(c2[0],a2[0]+1)):
+            x = True
+        else:
+            x = False
+    return x
 
-if (a1[0] in range(a2[0],c2[0]+1)) and (a1[1] in range(a2[1],c2[1]+1)):
-    print("it overlapps")
+def compare_y(a1,a2,c2):
+    if a2[1]<c2[1]:
+        if (a1[1] in range(a2[1],c2[1]+1)):
+            y = True
+        else:
+            y = False
+    if a2[1]>c2[1]:
+        if (a1[1] in range(c2[1],a2[1]+1)):
+            y = True
+        else:
+            y = False
+    return y
+
+a1 = []
+c1 = []
+a2 = []
+c2 = []
+z = [a1,c1,a2,c2]
+prompt = ["a1","c1","a2","c2"]
+for i in range(0,4):
+    x = int(input("coordinates of angle %s\n"%prompt[i]))
+    y = int(input())
+    z[i].append(x)
+    z[i].append(y)
+
+print(a1,c1,a2,c2)
+
+a = (compare_x(a1,a2,c2)) and (compare_y(a1,a2,c2))
+b = (compare_x(c1,a2,c2)) and (compare_y(c1,a2,c2))
+c = (compare_x(a1,a2,c2)) and (compare_y(c1,a2,c2))
+d = (compare_x(c1,a2,c2)) and (compare_y(a1,a2,c2))
+
+if (a or b or c or d) == True:
+    print("the rectangles overlap")
 else:
-    print("1")
-if (c1[0] in range(a2[0],c2[0]+1)) and (c1[1] in range(a2[1],c2[1]+1)):
-    print("it overlapps")
-else:
-    print("2")
-    
-    
+    print("the rectangles don't overlap")
     
