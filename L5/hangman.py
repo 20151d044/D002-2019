41a11c69ccd9e1da2685cd32099e680ef26d7a7b
20151d044 <53845659+20151d044@users.noreply.github.com>
2019-08-16 02:54:02 +0800

    Update hangman.py

--- L5/hangman.py
+++ L5/hangman.py
@@ -1,13 +1,22 @@
 import random
 word_list = ["apple","egg","carrot","bread","cat","library"]
 secret = word_list[random.randint(0,len(word_list)-1)]
+opened = []
 
-for i in range (0,len(secret)):
-    print("_",end="")
+opened.append("_"*len(secret))
 
-def checker(letter):
+def checker(secret,letter):
     for i in range (0,len(secret)):
-        if secret[i] == letter and 
-            list.append(letter)
+        if secret[i] == letter : 
+            opened[i] = letter
+    return opened
+
+print(opened)
+while opened != secret:
+    letter = input("input a letter")
+    checker(secret,letter)
+    print(opened)
+
+print("cpngratulaitons, you won!")
 
 
