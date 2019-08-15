2be9f30ae8452a9ab37aa593345924418c917549
20151d044 <53845659+20151d044@users.noreply.github.com>
2019-08-15 23:04:13 +0800

    Update Q2.py

--- L5/Q2.py
+++ L5/Q2.py
@@ -24,42 +24,30 @@ def check_row():
 
 def check_diagonal():
     for i in range(0,2):
-        if cells[0][0] == cells[1][1] == cells[2][2] != ' 'or cells[0][2] == cells[1][1] == cells[2][0]:
+        if cells[0][0] == cells[1][1] == cells[2][2] != ' 'or cells[0][2] == cells[1][1] == cells[2][0] != ' ':
             return True
         elif i == 1:
             return False
-full = False
-printer()
-p = ["x","o"]
-while (check_col() and check_row() and check_diagonal()) == False and full == False:
-    for a in range(0,2):
-        for b in range (0,3):
-            for c in range (0,3):
-                if cells[b][c] == ' ':
-                    full = False
-        if ((check_col() and check_row() and check_diagonal()) == False) and (full == False):
-            row = int(input("you are %s, please place your symbol\nrow ="%p[a]))
-            col = int(input("column ="))
-            while cells[row-1][col-1] != " " or (row or col) not in range(1,4):
-                print("invalid input")
-                row = int(input("row ="))
-                col = int(input("column ="))
-                if cells[row-1][col-1] == " ":
-                    break
-            cells[row-1][col-1] = p[a]
-            printer()
-        else:
-            break
-    
+def check():
+    if check_col() or check_row() or check_diagonal() == True:
+        return True
+    else:
+        return False
 
-if full != False:
+p = ["x","o"]
+n = 1
+while check() == False and n <= 9:
+    printer()
+    row = int(input("you are %s, please place your symbol\nrow ="%p[n%2]))
+    col = int(input("column ="))
+    while cells[row][col] != " " or (row or col) not in range(0,3):
+        print("invalid input")
+        row = int(input("row ="))
+        col = int(input("column ="))
+    cells[row][col] = p[n%2]
+    n = n+1
+
+if n == 10:
     print("draw")
 else:
-    print("game over")
-
-
-        
-
-    
-
-
+    print("game over, %s wins"%p[n%2])
